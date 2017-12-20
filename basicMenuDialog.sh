b40cbd53a2d99741385e5d94eae6c8950dd07b98
Martin Gaido <martin.gaido@gmail.com>
2017-12-20 09:40:37 -0300

    Basic text and dialog (ncurses) menues

---
+++ basicMenuDialog.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Using dialog to create a menu
+
+temp1=$(mktemp -t test1.XXXXX)
+temp2=$(mktemp -t test2.XXXXX)
+
+function diskspace {
+    df -k > $temp1
+    dialog --textbox $temp1 20 60
+}
+
+function whoseon {
+    who > $temp1
+    dialog --textbox $temp1 20 50
+}
+
+function memusage {
+    
+    # Uncomment this line if you're using MacOSX
+    # top -l 1 -s 0 | awk ' /Processes/ || /PhysMem/ || /Load Avg/{print}'
+    
+    # This like will work with GNU/Linux systems
+    cat /proc/meminfo > $temp1
+    dialog --textbox $temp1 20 50
+}
+
+while [ 1 ]
+    do
+        dialog --menu "Sys Admin Menu" 20 30 10 1
+                      "Display Disk Space" 2
+                      "Display Users" 3
+                      "Display Memory Usage" 0
+                      "Exit" 2> $temp2
+    if [ $? -eq 1 ]
+    then
+        break
+    fi
+
+selection=$(cat $temp2)
+
+case $selection in
+    1)
+        diskspace ;;
+    2)
+        whoseon ;;
+    3)
+        memusage ;;
+    0)
+        break ;;
+    *)
+        dialog --msgbox "Sorry, invalid selection" 10 30
+    esac
+done
+rm -f $temp1 2> /dev/null
+rm -f $temp2 2> /dev/null
---
+++ basicTextMenu.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Simple Script Menu
+
+function diskspace {
+    clear
+    df -k
+}
+
+function whoseon {
+    clear
+    who
+}
+
+function memusage {
+    clear
+    cat /proc/meminfo
+
+    # Uncomment this like if you're using MacOSX
+    # top -l 1 -s 0 | awk ' /Processes/ || /PhysMem/ || /Load Avg/{print}'
+}
+
+function menu {
+    clear
+    echo
+    echo -e "\t\t\tSys Admin Menu\n"
+    echo -e "\t1. Display Disk Space"
+    echo -e "\t2. Display Loggued On Users"
+    echo -e "\t3. Display Memory Usage"
+    echo -e "\t0. Exit Menu\n\n"
+    echo -en "\t\tEnter Option: "
+    read -n 1 option
+}
+
+while [ 1 ]
+do
+    menu
+    case $option in
+        0)
+            break ;;
+        1)
+            diskspace ;;
+        2)
+            whoseon ;;
+        3)
+            memusage ;;
+        *)
+            clear
+            echo "Sorry, wrong selection";;
+    esac
+    echo -en "\n\n\t\t\tHit any key to continue"
+    read -n 1 line
+done
+clear
