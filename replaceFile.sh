958a280cb5086fc13f3257f2e5b86d8cd49dc0d0
Martin Gaido <martin.gaido@gmail.com>
2017-12-22 15:54:55 -0300

    Make some improvements on replaceFile.sh

--- replaceFile.sh
+++ replaceFile.sh
@@ -8,7 +8,7 @@ read file_dest
 
 if [ -f "$file_orig" ]
 then
-    sed 's/(//;s/)//;s/-//;s/\.//;s/ //;s/-//;s/\.//;s/ //;s/,//;s/\"//;s/_//;s/+//' $file_orig > $file_dest
+    sed 's/(//g;s/)//g;s/-//g;s/\.//g;s/ //g;s/,//g;s/\"//g;s/_//g;s/+//g;s/;//g;s/\r//g;s/\n//g;s/\r\n//g;s/[^0-9]*//g;s/[[:blank:]]//g;s/^[1]//g;s/\s+//g;/^$/d' $file_orig > $file_dest
     echo "A new file has been created:" $file_dest
     echo ""
     cat $file_dest
